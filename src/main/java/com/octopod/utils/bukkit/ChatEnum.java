d74215b77f17cc81c79dcf84f6e7f6063bd3c52b
Octopod <octopodsquad@gmail.com>
2014-01-31 13:12:23 -1000

    added ChatEnum class

---
+++ src/main/java/com/octopod/utils/bukkit/ChatEnum.java
@@ -0,0 +1,21 @@
+package com.octopod.utils.bukkit;
+
+public class ChatEnum {
+	
+	public enum alignment {
+		LEFT, RIGHT, CENTER
+	}
+	
+	public enum flags {
+		UNPRECISE, SKIPRIGHT
+	}
+	
+	public enum clickEvent {
+		OPEN_URL, OPEN_FILE, RUN_COMMAND, SUGGEST_COMMAND
+	}
+	
+	public enum hoverEvent {
+		SHOW_TEXT, SHOW_ACHIEVEMENT, SHOW_ITEM
+	}
+
+}
