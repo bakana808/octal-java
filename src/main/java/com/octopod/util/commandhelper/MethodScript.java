337b3c1f93f50d43e151f4f42e2ca82d918ce02e
Octopod <octopodsquad@gmail.com>
2014-10-21 05:14:52 -1000

    updated MethodScript library MethodScript renamed to CommandHelper CompiledMethodScript renamed to MethodScript started a MethodScriptBuilder class moved all Environment-related functions into a new class, MethodScriptEnvironment (just a Environment wrapper)

--- src/main/java/com/octopod/util/commandhelper/MethodScript.java
+++ src/main/java/com/octopod/util/commandhelper/MethodScript.java
@@ -1,423 +1,222 @@
 package com.octopod.util.commandhelper;
 
 import com.laytonsmith.abstraction.MCCommandSender;
-import com.laytonsmith.commandhelper.CommandHelperFileLocations;
-import com.laytonsmith.commandhelper.CommandHelperPlugin;
 import com.laytonsmith.core.MethodScriptCompiler;
+import com.laytonsmith.core.MethodScriptComplete;
 import com.laytonsmith.core.ParseTree;
-import com.laytonsmith.core.Procedure;
-import com.laytonsmith.core.Profiles;
-import com.laytonsmith.core.Profiles.InvalidProfileException;
-import com.laytonsmith.core.constructs.*;
-import com.laytonsmith.core.environments.CommandHelperEnvironment;
+import com.laytonsmith.core.constructs.Construct;
+import com.laytonsmith.core.constructs.Target;
 import com.laytonsmith.core.environments.Environment;
-import com.laytonsmith.core.environments.GlobalEnv;
 import com.laytonsmith.core.exceptions.ConfigCompileException;
 import com.laytonsmith.core.exceptions.ConfigCompileGroupException;
-import com.laytonsmith.core.taskmanager.TaskManager;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 /**
- * @author Octopod
- *         Created on 5/24/14
+ * @author Octopod Created on 5/24/14
+ */
+
+/**
+ * An object representing the compiled MethodScript.
  */
 public class MethodScript
 {
 	/**
-	 * Compiles MethodScript from a string.
+	 * The environment that the script will use.
+	 */
+	private MethodScriptEnvironment environment = null;
+
+	/**
+	 * If false, a clone of the environment will be used in place of the real one so as to not save potential
+	 * environment changes.
+	 */
+	private boolean dynamicEnv = false;
+
+	/**
+	 * The compiled CompiledMethodScript.
+	 */
+	private ParseTree compiled;
+
+	private void compile(String script, File source, MethodScriptEnvironment env, boolean autorun)
+	throws ConfigCompileException, ConfigCompileGroupException
+	{
+		//Sets the source to UNKNOWN if null
+		if (source == null) source = Target.UNKNOWN.file();
+
+		//Sets the environment to a default environment if null
+		if (env == null) {
+			this.environment = new MethodScriptEnvironment();
+		} else {
+			this.environment = env;
+		}
+
+		compiled = MethodScriptCompiler.compile(MethodScriptCompiler.lex(script, source, true));
+
+		if(autorun) execute();
+	}
+
+	@Deprecated
+	/**
+	 * Compiles MethodScript using a File as a source
+	 * @deprecated Use the constructor with MethodScriptEnvironment instead (<code>new MethodScriptEnvironment(Environment)</code>)
+	 */
+	public MethodScript(String script, Environment env) throws ConfigCompileException, ConfigCompileGroupException
+	{
+		compile(script, null, new MethodScriptEnvironment(env), false);
+	}
+
+
+	/**
+	 * Compiles CompiledMethodScript using UNKNOWN as the source. (just like the interpreter)
 	 *
-	 * @param script the script to compile
-	 * @return the compiled script
+	 * @param script The CompiledMethodScript to compile
 	 *
 	 * @throws ConfigCompileException
-	 * @throws ConfigCompileGroupException
 	 */
-	public static CompiledMethodScript compile(String script) throws ConfigCompileException, ConfigCompileGroupException
+	public MethodScript(String script) throws ConfigCompileException, ConfigCompileGroupException
 	{
-		return new CompiledMethodScript(script);
+		compile(script, null, null, false);
 	}
 
-	public static ParseTree compileParseTree(String script) throws ConfigCompileException, ConfigCompileGroupException
+	public MethodScript(String script, MethodScriptEnvironment env) throws ConfigCompileException, ConfigCompileGroupException
 	{
-		return MethodScriptCompiler.compile(MethodScriptCompiler.lex(script, Target.UNKNOWN.file(), true));
+		compile(script, null, env, false);
+	}
+
+	public MethodScript(String script, boolean autorun) throws ConfigCompileException, ConfigCompileGroupException
+	{
+		compile(script, null, null, autorun);
 	}
 
 	/**
-	 * Compiles MethodScript from a string with an environment.
+	 * Compiles CompiledMethodScript from a File, using the File as the source.
 	 *
-	 * @param script the script to compile
-	 * @param env the environment to use
-	 * @return the compiled script
+	 * @param file The file to read CompiledMethodScript from.
 	 *
-	 * @throws ConfigCompileException
-	 * @throws ConfigCompileGroupException
-	 */
-	public static CompiledMethodScript compile(String script, Environment env) throws ConfigCompileException, ConfigCompileGroupException
-	{
-		return new CompiledMethodScript(script, null, env);
-	}
-
-	/**
-	 * Runs CompiledMethodScript, and returns the resultant Construct. (as the console by default)
-	 * May throw ConfigCompileException during the compiling stage.
-	 *
-	 * @param script The CompiledMethodScript to run.
-	 * @return Construct
-	 *
+	 * @throws java.io.IOException
 	 * @throws com.laytonsmith.core.exceptions.ConfigCompileException
 	 */
-	public static Construct eval(String script) throws ConfigCompileException, ConfigCompileGroupException
+	public MethodScript(File file, MethodScriptEnvironment env) throws IOException, ConfigCompileException, ConfigCompileGroupException
 	{
-		return eval(script, null, null);
+		compile(read(file), file, env, false);
 	}
 
-	public static Procedure proc(String name, List<IVariable> vars, String script) throws ConfigCompileException, ConfigCompileGroupException
+	public MethodScript(File file) throws IOException, ConfigCompileException, ConfigCompileGroupException
 	{
-		return new Procedure(name, CClassType.AUTO, vars, compileParseTree(script), Target.UNKNOWN);
+		compile(read(file), file, null, false);
 	}
 
-	/**
-	 * Runs CompiledMethodScript, and returns the resultant Construct.
-	 * May throw ConfigCompileException during the compiling stage.
-	 * @param script The CompiledMethodScript to run.
-	 * @param executor Who this script is going to be executed by.
-	 * @return Construct
-	 * @throws com.laytonsmith.core.exceptions.ConfigCompileException
-	 */
-	public static Construct eval(String script, MCCommandSender executor) throws ConfigCompileException, ConfigCompileGroupException
+	public MethodScript(File file, boolean autorun) throws IOException, ConfigCompileException, ConfigCompileGroupException
 	{
-		return eval(script, null, executor);
+		compile(read(file), file, null, autorun);
 	}
 
-	public static Construct eval(String script, IVariableList variables, MCCommandSender executor) throws ConfigCompileException, ConfigCompileGroupException
+	private static String read(File file) throws IOException
 	{
-		CompiledMethodScript ms = new CompiledMethodScript(script);
+		final StringBuilder sb = new StringBuilder();
 
-		if(variables != null) {
-			ms.setVar(variables);
-		}
-		if(executor != null) {
-			ms.setExecutor(executor);
-		}
+		BufferedInputStream input = new BufferedInputStream(new FileInputStream(file));
 
-		return ms.execute();
-	}
-
-	private static Environment defaultEnvironment = null;
-
-    /**
-     * Gets CommandHelper's default environment.
-     * @return Environment
-     */
-    public static Environment createEnvironment()
-	{
-		if(defaultEnvironment != null) {
-			return cloneEnvironment(defaultEnvironment);
-		}
-
-        CommandHelperPlugin plugin = CommandHelperPlugin.self;
-        GlobalEnv gEnv;
-
-        try
+		int n;
+		while ((n = input.read()) != -1)
 		{
-            gEnv = new GlobalEnv(
-                    plugin.executionQueue,
-                    plugin.profiler,
-                    plugin.persistenceNetwork,
-                    plugin.permissionsResolver,
-                    CommandHelperFileLocations.getDefault().getConfigDirectory(),
-                    new Profiles(CommandHelperFileLocations.getDefault().getProfilesFile()),
-					new TaskManager()
-            );
-        }
-		catch (InvalidProfileException | IOException e)
-		{
-			return cloneEnvironment(defaultEnvironment);
+			sb.append((char) n);
 		}
 
-		gEnv.SetDynamicScriptingMode(true);
-        CommandHelperEnvironment cEnv = new CommandHelperEnvironment();
+		input.close();
 
-        return defaultEnvironment = Environment.createEnvironment(gEnv, cEnv);
+		return sb.toString();
+	}
 
-    }
-
-	/**
-	 * Attempts to clone the Environment, and if it fails, returns null.
-	 * @param env
-	 * @return
-	 */
-	public static Environment cloneEnvironment(Environment env)
+	public MethodScriptEnvironment getEnvironment()
 	{
-		try {
-			return env.clone();
-		} catch (CloneNotSupportedException e) {
-			return null;
-		}
+		return environment;
 	}
 
-	public static Environment addVariable(Environment env, IVariable var) {
-		IVariableList vars = env.getEnv(GlobalEnv.class).GetVarList();
-		vars.set(var);
-		env.getEnv(GlobalEnv.class).SetVarList(vars);
-		return env;
-	}
-
-	public static Construct getVariable(Environment env, String varName) {
-		IVariableList vars = env.getEnv(GlobalEnv.class).GetVarList();
-		return vars.get(varName, Target.UNKNOWN).ival();
-	}
-
-	/**
-	 * Extracts the ParseTree variable from a Procedure
-	 * @param procedure
-	 * @return
-	 */
-	public static ParseTree getProcedureTree(Procedure procedure)
+	public void setDynamicEnv(boolean dyn)
 	{
-		try
-		{
-			Field field = procedure.getClass().getDeclaredField("tree");
-			field.setAccessible(true);
-			return (ParseTree)field.get(procedure);
-		}
-		catch (NoSuchFieldException | IllegalAccessException e)
-		{
-			e.printStackTrace();
-			return null;
-		}
+		dynamicEnv = dyn;
 	}
 
-	/**
-	 * Extracts the original variables from a Procedure
-	 * @param procedure
-	 * @return
-	 */
-	@SuppressWarnings("unchecked")
-	public static Map<String, Construct> getProcedureVars(Procedure procedure) {
-		try {
-			Field field = procedure.getClass().getDeclaredField("originals");
-			field.setAccessible(true);
-			return (Map<String, Construct>)field.get(procedure);
-		} catch (NoSuchFieldException | IllegalAccessException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	private static <T> T newInstance(Class<T> type)
+	public boolean isEnvironmentDynamic()
 	{
-		try {
-			return type.getConstructor().newInstance();
-		} catch (Exception e) {
-			return null;
-		}
+		return dynamicEnv;
+	}
+
+	public String getSource()
+	{
+		return getTarget().file().toString();
+	}
+
+	public Target getTarget()
+	{
+		return compiled.getTarget();
+	}
+
+	public Construct execute(MCCommandSender executor)
+	{
+		environment.setExecutor(executor);
+		return execute();
+	}
+
+	public Construct execute(MCCommandSender executor, Environment env)
+	{
+		environment.setExecutor(executor);
+		return execute(env);
+	}
+
+	public Construct execute()
+	{
+		return execute((MCCommandSender)null, null);
+	}
+
+	public Construct execute(MethodScriptComplete done)
+	{
+		return execute(done, null);
+	}
+
+	public Construct execute(Environment env)
+	{
+		return execute((MCCommandSender) null, env);
 	}
 
 	/**
-	 * Converts a Java object into a Construct
+	 * Executes this code.
 	 *
-	 * @param object the Java object
-	 * @return the CommandHelper construct
+	 * @param done        this will run after the code is done, can be null
+	 * @param externalEnv the execution will use this environment if provided, can be null
+	 *
+	 * @return the Construct that results from this code
 	 */
-	public Construct toConstruct(Object object)
+	public Construct execute(MethodScriptComplete done, Environment externalEnv)
 	{
-		Target t = Target.UNKNOWN;
+		Environment env;
+		if (externalEnv == null)
+			//Use our environment
+			env = environment.getHandle();
+		else
+			//Use the external environment; don't save procedures if this happens
+			env = externalEnv;
 
-		if(object == null) return CNull.NULL;
+		MethodScriptCompiler.registerAutoIncludes(environment.getHandle(), null);
 
-		if(object instanceof Byte) return toConstruct(((Byte) object).longValue());
-		if(object instanceof Integer) return toConstruct(((Integer) object).longValue());
-		if(object instanceof Short) return toConstruct(((Short) object).longValue());
-		if(object instanceof Long) return new CInt((long)object, t);
-
-		if(object instanceof Float) return toConstruct(((Float) object).doubleValue());
-		if(object instanceof Double) return new CDouble((double)object, t);
-
-		if(object instanceof Boolean) return CBoolean.get((boolean)object);
-
-		if(object instanceof Character) return new CString((char)object, t);
-		if(object instanceof String) return new CString((String)object, t);
-
-		if(object instanceof int[])
-		{
-			CArray array = new CArray(t);
-			for(int o: (int[])object) array.push(toConstruct(o));
-			return array;
-		}
-
-		if(object instanceof byte[])
-		{
-			CArray array = new CArray(t);
-			for(byte o: (byte[])object) array.push(toConstruct(o));
-			return array;
-		}
-
-		if(object instanceof short[])
-		{
-			CArray array = new CArray(t);
-			for(short o: (short[])object) array.push(toConstruct(o));
-			return array;
-		}
-
-		if(object instanceof long[])
-		{
-			CArray array = new CArray(t);
-			for(long o: (long[])object) array.push(toConstruct(o));
-			return array;
-		}
-
-		if(object instanceof float[])
-		{
-			CArray array = new CArray(t);
-			for(float o: (float[])object) array.push(toConstruct(o));
-			return array;
-		}
-
-		if(object instanceof double[])
-		{
-			CArray array = new CArray(t);
-			for(double o: (double[])object) array.push(toConstruct(o));
-			return array;
-		}
-
-		if(object instanceof boolean[])
-		{
-			CArray array = new CArray(t);
-			for(boolean o: (boolean[])object) array.push(toConstruct(o));
-			return array;
-		}
-
-		if(object instanceof char[])
-		{
-			CArray array = new CArray(t);
-			for(char o: (char[])object) array.push(toConstruct(o));
-			return array;
-		}
-
-		if(object instanceof Object[])
-		{
-			CArray array = new CArray(t);
-			for(Object o: (Object[])object) array.push(toConstruct(o));
-			return array;
-		}
-
-		if(object instanceof Collection)
-		{
-			return toConstruct(((Collection) object).toArray());
-		}
-
-		if(object instanceof Map)
-		{
-			Map map = (Map)object;
-			CArray array = new CArray(t);
-			for(Object key: map.keySet()) array.set(key.toString(), toConstruct(map.get(key)), t);
-			return array;
-		}
-
-		CArray array = new CArray(t);
-
-		for(Field field: object.getClass().getDeclaredFields())
-		{
-			boolean wasUnlocked = field.isAccessible();
-			field.setAccessible(true);
-			try {
-				array.set(field.getName(), toConstruct(field.get(object)), t);
-			} catch (IllegalAccessException e) {}
-			field.setAccessible(wasUnlocked);
-		}
-		return array;
+		return MethodScriptCompiler.execute(compiled, env, done, null);
 	}
 
-	/**
-	 * Converts a Construct into a Java object.
-	 *
-	 * @param construct the commandhelper construct
-	 * @param type the type to convert to
-	 *
-	 * @return the object matching type
-	 */
-	@SuppressWarnings("unchecked")
-	public <T> T fromConstruct(Construct construct, Class<T> type)
+	public Thread executeAsync(final MethodScriptComplete done)
 	{
-		Object object = null;
-
-		if(construct instanceof CInt) object = ((CInt)construct).getInt();
-		if(construct instanceof CDouble) object = ((CDouble)construct).getDouble();
-		if(construct instanceof CBoolean) object = ((CBoolean)construct).getBoolean();
-		if(construct instanceof CString) object = construct.val();
-
-		if(object instanceof Boolean)
-		{
-			Boolean x = (Boolean)object;
-			if(type == Boolean.class || type == boolean.class) return (T)x;
-		}
-		if(object instanceof Long)
-		{
-			Long x = (Long)object;
-			if(type == Integer.class || type == int.class) 		return (T)(Integer)x.intValue();
-			if(type == Short.class || type == short.class) 		return (T)(Short)x.shortValue();
-			if(type == Byte.class || type == byte.class) 		return (T)(Byte)x.byteValue();
-			if(type == Long.class || type == long.class)		return (T)x;
-		}
-		if(object instanceof Double)
-		{
-			Double x = (Double)object;
-			if(type == Float.class || type == float.class)		return (T)(Float)x.floatValue();
-			if(type == Double.class || type == double.class)	return (T)x;
-		}
-		if(object instanceof String)
-		{
-			String x = (String)object;
-			if(type == Character.class || type == char.class)	return (T)(Character)x.charAt(0);
-			if(type == String.class)	return (T)x;
-		}
-
-		if(construct instanceof CArray)
-		{
-			//Deserialize class fields???
-			CArray array = (CArray)construct;
-			Target t = Target.UNKNOWN;
-			T obj = newInstance(type);
-			if(obj == null) {return null;}
-
-			for(Construct key: array.keySet())
-			{
-				Field field;
-				boolean wasUnlocked;
-				try {
-					field = type.getDeclaredField(key.val());
-					wasUnlocked = field.isAccessible();
-
-					field.setAccessible(true);
-					field.set(obj, fromConstruct(array.get(key, t), field.getType()));
-					field.setAccessible(wasUnlocked);
-				} catch (NoSuchFieldException | IllegalAccessException e) {}
-			}
-			return obj;
-		}
-
-		return nonNullObject(type);
-
-
+		Thread thread = new Thread() {
+			public void run() {execute(done);}
+		};
+		thread.start();
+		return thread;
 	}
 
-	@SuppressWarnings("unchecked")
-	public static <T> T nonNullObject(Class<T> type)
+	public Thread executeAsync()
 	{
-		if(type == Integer.class || type == int.class) 		return (T)(Integer)(int)-1;
-		if(type == Short.class || type == short.class) 		return (T)(Short)(short)-1;
-		if(type == Byte.class || type == byte.class) 		return (T)(Byte)(byte)-1;
-		if(type == Long.class || type == long.class)		return (T)(Long)(long)-1;
-		if(type == Boolean.class || type == boolean.class) 	return (T)(Boolean)false;
-		if(type == Float.class || type == float.class)		return (T)(Float)(float)-1;
-		if(type == Double.class || type == double.class)	return (T)(Double)(double)-1;
-		if(type == Character.class || type == char.class)	return (T)(Character)' ';
-		if(type == String.class)							return (T)"";
-		return null;
+		return executeAsync(null);
 	}
 }
--- src/main/java/com/octopod/util/commandhelper/README.md
+++ src/main/java/com/octopod/util/commandhelper/README.md
@@ -7,55 +7,61 @@ Credits to LadyCailin and the rest of CommandHelper's contributors.
 
 Obviously, CommandHelper and whatever API (like Bukkit) that loaded it must be loaded.
 
-Examples
+MethodScript evaluation/compiling
 ------
-Evaluate code (as console):
+Evaluate code (statically as console):
 ```java
-MethodScript.eval("broadcast('hello')");
+CommandHelper.eval("broadcast('hello')");
 ```
 
-Evaluate code (as player):
+Evaluate code (statically as player):
 ```java
-MethodScript.eval("msg('hello')", new BukkitMCPlayer(player));
+CommandHelper.eval("msg('hello')", new BukkitMCPlayer(player));
+```
+
+Execute code (from MethodScript):
+```java
+MethodScript A = CommandHelper.compile("broadcast('hello')");
+A.execute();
+//with player
+MethodScript B = CommandHelper.compile("broadcast('goodbye')");
+B.execute(new BukkitMCPlayer(player));
 ```
 
 Compile code:
 ```java
-CompiledMethodScript A = MethodScript.compile("broadcast('hello')");
-//or
-CompiledMethodScript B = new CompiledMethodScript("broadcast('hello')");
+//with static CommandHelper.compile() method:
+MethodScript A = CommandHelper.compile("broadcast('hello')");
+//or using MethodScript's constructor:
+MethodScript B = new MethodScript("broadcast('goodbye')");
 ```
 
-Set variable in CompiledMethodScript A (all variable names start with @):
+MethodScript Environment Editing
+------
+Set variable in MethodScript A(all variable names start with @):
 ```java
-ms.setVariable("@a", new CInt(5, Target.UNKNOWN));
+MethodScript A = new MethodScript("broadcast(@a)");
+A.getEnvironment().setVariable("@a", new CInt(5, Target.UNKNOWN));
 ```
 
-Set procedure in CompiledMethodScript(all procedure names start with _):
+Set procedure in a MethodScript(all procedure names start with _):
 ```java
-Procedure proc = MethodScript.proc("_something", new ArrayList<IVariable>(), "broadcast('hello')");
-ms.include(proc);
-```
-
-Evaluate code (from CompiledMethodScript):
-```java
-ms.execute();
-//with player
-ms.execute(new BukkitMCPlayer(player));
-```
+MethodScript A = new MethodScript("_something('hello')");
+Procedure proc = CommandHelper.proc("_something", new ArrayList<IVariable>(), "broadcast('hello')");
+A.getEnvironment().include(proc);
 
 Setting a variable before executing CompiledMethodScript A:
 ```java
-CompiledMethodScript A = new CompiledMethodScript("broadcast(@hello)");
-
-A.setVar("@hello", new CString("hello", A.getTarget()));
+MethodScript A = new MethodScript("broadcast(@hello)");
+A.getEnvironment().setVariable("@hello", new CString("hello", A.getTarget()));
 ```
 
 Including procedures from CompiledMethodScript A in CompiledMethodScript B:
 ```java
-CompiledMethodScript A = new CompiledMethodScript("proc _hello() {broadcast('hello')}");
-CompiledMethodScript B = new CompiledMethodScript("_hello()");
+MethodScript A = new MethodScript("proc _hello() {broadcast('hello')}");
+MethodScript B = new MethodScript("_hello()");
 
 //The script needs to be executed at least once to get the procedures
-B.include(A.getProcedures());
+A.execute();
+B.include(A.getEnvironment().getProcedures());
 ```
